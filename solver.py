ce52c9dd8f5acacff4ee562f3cb91d1703c11c57
stefan <stef-ljt@berkeley.edu>
2019-12-02 13:23:00 -0800

    merge

--- solver.py
+++ solver.py
@@ -6,8 +6,8 @@ sys.path.append('../..')
 import argparse
 import utils
 
-from student_utils import *
 from graph import *
+from student_utils import *
 from prims import *
 
 """
@@ -30,8 +30,12 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
+    g = recoverGraph(list_of_locations, list_of_homes, adjacency_matrix)
+    mst1 = prims(g, starting_car_location)
+    mst2, d = dropOff(mst1, list_of_locations)
+    visited = []
 
-    return g
+    return d
 
 
 # adj matrix is a 2d list
@@ -70,6 +74,12 @@ def dropOff(mst, list_of_locations):
     return mst, dropOff_and_homes
 
 
+def dfs(visited, graph, node, path):
+    path.append(node)
+    if node not in visited:
+        visited.append(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
 
 """
 ======================================================================
